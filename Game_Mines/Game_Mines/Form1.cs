cdc0a1cd68779c4b128dcd68a76e8c7f25a161d1
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-04-17 00:10:52 +0700

    Проверяем бомба в поле или нет

--- Game_Mines/Game_Mines/Form1.cs
+++ Game_Mines/Game_Mines/Form1.cs
@@ -13,6 +13,8 @@ namespace Game_Mines
 {
     public partial class Form1 : Form
     {
+        Dictionary<Button, Element> _dictionary = new Dictionary<Button, Element>();
+
         public Form1()
         {
             InitializeComponent();
@@ -34,6 +36,8 @@ namespace Game_Mines
 
                     if (element.IsBomb) btn.Text = "b";
 
+                    _dictionary[btn] = element;
+
                     panel.Controls.Add(btn);
                 }
             }
@@ -43,9 +47,22 @@ namespace Game_Mines
 
         private void Btn_Click(object sender, MouseEventArgs e)
         {
+            var btn = (Button)sender;
+            var element = _dictionary[btn];
+
             if (e.Button == MouseButtons.Left)
             {
-                
+                if (element.IsBomb) MessageBox.Show("Bomb !!!");
+                else
+                {
+                    var count = 0;
+                    foreach (var currElement in _dictionary.Values)
+                    {
+                        if (Math.Abs(currElement.X - element.X) <= 1 && Math.Abs(currElement.Y - element.Y) <= 1 && currElement.IsBomb) count++;
+                    }
+
+                    btn.Text = count.ToString();
+                }
             }
             else if (e.Button == MouseButtons.Right)
             {
